c1407354c5fb665a210d0c27de3cdd49277c3135
Abdelwahab <ab28fb@gmail.com>
2023-01-06 12:33:35 +0100

    feature: screen recorder

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import numpy as np
 import cv2
 from PIL import ImageGrab
-import time
+import datetime
 
 # def main() -> None:
 # specify a unique name for the video
-file_name: str = time.localtime+"-record.mp4"
-# specify the width and height of screen
-width: int = 900
-height: int = 700
+file_name: str = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+file_name = file_name + ".mp4"
 # specify the resolution
-resolution = (width, height)
+# specify the width and height of screen
+image = ImageGrab.grab()
+width, height = image.size
 # specify the video codec
 codex = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-video = cv2.VideoWriter(file_name, codex, 20.0, resolution)
+# specify FPS
+fps = 15.0
+
+video = cv2.VideoWriter(file_name, codex, fps, (width, height))
 
 while (True):
-    image = ImageGrab.grab()
+    image = ImageGrab.grab(bbox=(0, 0, width, height))
     image_matrix = np.array(image)
     final_image = cv2.cvtColor(image_matrix, cv2.COLOR_BGR2RGB)
-    cv2.imshow("screen record", final_image)
     video.write(final_image)
 
     if (cv2.waitKey(10) == ord('q')):
